2edbe2403553831ccfa419f340f2a1a82bb899ba
Chiraag Balu <chiraag0729@outlook.com>
2020-10-29 13:31:35 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chiraag Balu on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
